079cb098feeae7f22afec1e5ec387d5d1a58241b
Gabi <gabrielaadalmaschio@gmail.com>
2026-01-12 22:30:37 -0300

    primeiro commit final

---
+++ main.py
@@ -0,0 +1,30 @@
+import os
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.all()
+intents.message_content = True
+
+bot = commands.Bot(
+    command_prefix="s.",
+    intents=intents,
+    help_command=None
+)
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user} está online!")
+
+@bot.command()
+async def help(ctx):
+    await ctx.send(
+        f"Olá, {ctx.author.mention}! "
+        "Sou a **Sophia**, a bot inspirada na Sophia do Katseye. "
+        "Meu prefixo é `s.`, fácil, né?"
+    )
+
+@bot.command()
+async def sophia(ctx):
+    await ctx.send(file=discord.File("banner.png"))
+
+bot.run(os.getenv("MTQ2MDM5Njc0MzA3NzM5NjY5Mw.Gsm9jj.EnyUvhX3kHBQdeu_b0y2k5890jIOYgq-BKTeV8"))
